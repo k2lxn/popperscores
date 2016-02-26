ac5b83bb4cffe54dca5d1c28c2ba5ffe0c7114b4
Jane <jane.e.reynolds@gmail.com>
2016-02-25 19:42:01 -0800

    Add custom site icon and display in header

--- wp-content/themes/popperscores/header.php
+++ wp-content/themes/popperscores/header.php
@@ -30,6 +30,25 @@
             <header id="masthead" class="site-header" role="banner">    
         <?php } ?>
  	
+        <?php // Display site icon or first letter as logo ?>
+            <div class="site-logo">
+                <?php $site_title = get_bloginfo('name'); ?>
+                <a href="<?php echo esc_url(home_url( '/' )); ?>" rel="home">
+                    <div class="screen-reader-text">
+                        <?php printf(esc_html__('Go to the homepage of %1$s', 'popperscores'), $site_title); ?>
+                    </div>
+                    <?php 
+                    if( has_site_icon() ) { 
+                        $site_icon = esc_url( get_site_icon_url( 270 )); ?>
+                        <img class="site-icon" src="<?php echo $site_icon; ?>" alt="">
+                    <?php } else { ?>
+                        <div class="site-firstletter" aria-hidden="true">
+                            <?php echo substr($site_title, 0, 1); ?>
+                        </div>
+                    <?php } ?>
+                </a>
+            </div>
+        
             <div class="site-branding">
                 <?php
                 if ( is_front_page() && is_home() ) : ?>
